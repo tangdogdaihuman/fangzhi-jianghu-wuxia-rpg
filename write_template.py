b7652dd76ddd306874dde20de811465ecce6de68
admin <admin@users.noreply.github.com>
2026-07-08 03:57:58 +0800

    Remove temp files from tracking and update .gitignore

--- write_template.py
+++
@@ -1,6 +0,0 @@
-
-import os
-html_content = open(os.path.join(os.path.dirname(__file__), "html_content.txt"), "r", encoding="utf-8").read()
-with open("templates/index.html", "w", encoding="utf-8") as f:
-    f.write(html_content)
-print("Done")
